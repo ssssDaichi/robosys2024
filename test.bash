69a9903eab24f9117f599da433021534f5be6e68
Daichi Hirose <marumaru09030903@gmail.com>
2024-10-31 13:03:02 +0900

    著作権表示入れた

---
+++ test.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2024 Daichi Hirose
+ ng () {
+         echo ${1}行目が違うよ
+         res=1
+ }　
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+ 　   　
+[ "${res}" = 0 ] && echo OK
+exit $res
