1a251a79abf156d441f65669f7ba1845470da61a
Daichi Hirose <marumaru09030903@gmail.com>
2024-11-07 13:02:00 +0900

    著作権表示入れた

--- test.bash
+++ test.bash
@@ -10,5 +10,14 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
-[ "${res}" = 0 ] && echo OK
+out=$(echo あ | ./plus)           
+[ "$?" = 1 ]      || ng "$LINENO" 
+[ "${out}" = "" ] || ng "$LINENO" 
+  　                                  
+out=$(echo | ./plus)              
+[ "$?" = 1 ]      || ng "$LINENO" 
+[ "${out}" = "" ] || ng "$LINENO"
+  　 
+[ "$res" = 0 ] && echo OK
 exit $res
+
